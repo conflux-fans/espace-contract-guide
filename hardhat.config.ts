6f3af5175d972299c62fe3dbda24cd68f69ebf8b
PanaW <pana.wang@outlook.com>
2023-11-23 10:56:19 +0800

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    espaceTestnet: {
+      url: process.env.ESPACE_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  sourcify: {
+    enabled: false,
+  },
+  etherscan: {
+    apiKey: {
+      espaceTestnet: 'espace',
+    },
+    customChains: [
+      {
+        network: 'espaceTestnet',
+        chainId: 71,
+        urls: {
+          apiURL: 'https://evmapi-testnet.confluxscan.io/api/',
+          browserURL: 'https://evmtestnet.confluxscan.io/',
+        },
+      },
+    ],
+  },
 };
 
 export default config;
