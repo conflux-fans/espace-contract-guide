d9fe1a494cc97aed4a0edcaf70128e54faacccb2
Pana <pana.wang@outlook.com>
2024-07-17 09:51:48 +0800

    op import

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
-import * as dotenv from "dotenv";
-dotenv.config();
+import  "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
